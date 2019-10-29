f8c67aedd401db897088bfa0e3f3107c53eb3055
Orest Tarasiuk <orest@tarasiuk.me>
2019-10-29 17:53:22 +0100

    options wip

--- options.js
+++ options.js
@@ -1,17 +1,37 @@
 'use strict';
 
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
-      })
-    });
-    page.appendChild(button);
+let presetForm = document.getElementById('presetForm');
+let presetFormSubmitButton = document.getElementById('presetFormSubmitButton');
+
+var calendars = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+
+function constructOptions(calendars) {
+  for (let calendar of calendars) {
+    let div = document.createElement('div');
+    let inputPreset1 = document.createElement('input');
+    let slash = document.createElement('span')
+    let inputPreset2 = document.createElement('input');
+    let span = document.createElement('span');
+
+    inputPreset1.type = "checkbox";
+    inputPreset1.name = calendar;
+    slash.textContent = "/";
+    inputPreset2.type = "checkbox";
+    inputPreset2.name = calendar;
+    span.textContent = calendar;
+
+    div.appendChild(inputPreset1);
+    div.appendChild(slash);
+    div.appendChild(inputPreset2);
+    div.appendChild(span);
+    presetForm.prepend(div);
   }
+
+  presetFormSubmitButton.addEventListener('click', function() {
+    chrome.storage.sync.set({color: item}, function() {
+      console.log('color is ' + item);
+    })
+  });
 }
-constructOptions(kButtonColors);
+
+constructOptions(calendars);
