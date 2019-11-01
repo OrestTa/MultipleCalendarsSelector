d15ddf1db7eaa61d8ac05ff628e9a7df4210a140
Orest Tarasiuk <orest@tarasiuk.me>
2019-11-01 16:29:34 +0100

    unified options page

--- popup.js
+++ popup.js
@@ -54,5 +54,5 @@ presetNoneButton.onclick = function(element) {
 };
 
 openOptionsButton.onclick = function(element) {
-  chrome.tabs.create({ 'url': 'chrome://extensions/?options=' + chrome.runtime.id });
+  chrome.tabs.create({ 'url': 'chrome-extension://' + chrome.runtime.id + '/options.html' });
 }
