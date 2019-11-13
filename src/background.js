947cf0f97d27c796e429104d32ddaa3c17bff316
Orest Tarasiuk <orest@tarasiuk.me>
2019-11-13 17:11:22 +0100

    detect extension upgrades

--- src/background.js
+++ src/background.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const currentPackageVersion = 1.5; // TODO
+const storageIdForLastSeenPackageVersion = "lastSeenPackageVersion";
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.tabs.create({ url: googleCalendarUrl });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
@@ -12,6 +15,23 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
+function persistPackageVersion(versionToPersist, callback) {
+  console.log("Persisting version " + versionToPersist);
+  chrome.storage.sync.set({[storageIdForLastSeenPackageVersion]: versionToPersist}, callback);
+}
+
+function checkForVersionUpgrade() {
+  chrome.storage.sync.get(storageIdForLastSeenPackageVersion, function(data) {
+    const lastSeenPackageVersion = data[storageIdForLastSeenPackageVersion];
+    if (lastSeenPackageVersion !== currentPackageVersion) {
+      tracker.sendEvent('Extension upgrade detected from-to', lastSeenPackageVersion, currentPackageVersion);
+      persistPackageVersion(currentPackageVersion);
+    }
+  });
+}
+
 var tracker = getAnalyticsTracker();
 tracker.sendAppView('BackgroundView');
-tracker.sendEvent('Background', 'Extension version', '1.5');
+tracker.sendEvent('Background', 'Extension version', currentPackageVersion);
+
+checkForVersionUpgrade();
