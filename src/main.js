0a0752f1e03f08343398b322fbbca4caf7043611
igortrsk <igor.tarasiuk4@gmail.com>
2025-08-06 13:28:56 +0200

    fixed query to get calendar name

--- src/main.js
+++ src/main.js
@@ -157,7 +157,7 @@ function getCalendarDivs() {
 
 function findCalendarsInDiv(div) {
     let foundCalendars = []
-    div.find('span[jsslot]:not([class])').each(function (index) {
+    div.find('span[jsslot]').each(function (index) {
         foundCalendars.push(jQuery(this).parent().parent())
     })
     return foundCalendars
